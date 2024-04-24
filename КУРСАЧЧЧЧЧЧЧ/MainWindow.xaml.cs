c95924047913fa1c2db8c1c0c8517f8c0bafcdad
Старчак Данил <starchak05@bk.ru>
2024-04-24 13:00:59 +0300

    Add .gitignore to ignore .vs directory

---
+++ КУРСАЧЧЧЧЧЧЧ/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace КУРСАЧЧЧЧЧЧЧ
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Page1 page1 = new Page1();
+            this.Content = page1;
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ КУРСАЧЧЧЧЧЧЧ/Support.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace КУРСАЧЧЧЧЧЧЧ
+{
+    /// <summary>
+    /// Логика взаимодействия для Support.xaml
+    /// </summary>
+    public partial class Support : Page
+    {
+        public Support()
+        {
+            InitializeComponent();
+        }
+
+        private void Jope_Click(object sender, RoutedEventArgs e)
+        {
+            string messageText = MSB.Text; // Получаем текст из TextBox
+
+            ContentControl bubble = new ContentControl
+            {
+                Content = new TextBlock { Text = messageText },  // Создаем TextBlock с текстом сообщения
+                Style = (Style)Resources["BubbleRightStyle"] // Используем стиль для правого пузырька
+            };
+
+            ChatMessages.Children.Add(bubble); // Добавляем пузырек с сообщением в чат
+        }
+    }
+}
